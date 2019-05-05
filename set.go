847a97d66932a89c91d73fdedc90028ed44fc7b9
Jimmy <zhoumin@bupt.edu.cn>
2019-05-05 22:54:04 +0800

    implement demo for set

--- set.go
+++ set.go
@@ -2,8 +2,18 @@ package set
 
 import "fmt"
 
-func Set() {
+type Set struct {
+	values map[string]int
+}
+
+func (s *Set) Add(val string) {
+	s.values[val] = 1
+}
+
+func New() *Set {
 	//fmt.Println("You have create a new Set!")
 	//fmt.Println("It just is a demo and updating...")
 	fmt.Println("I'm in set.Set()")
+	return Set{}
+
 }
--- set2.go
+++
@@ -1,9 +0,0 @@
-package set2
-
-import "fmt"
-
-func Set() {
-	//fmt.Println("You have create a new Set!")
-	//fmt.Println("It just is a demo and updating...")
-	fmt.Println("I'm in set2.Set()")
-}
