c4f9c56dacab0f3b05a8e1b78aff775634e49bbe
Jimmy <zhoumin@bupt.edu.cn>
2019-05-05 17:47:57 +0800

    add set2.go

--- set2.go
+++ set2.go
@@ -1,9 +1,9 @@
-package set
+package set2
 
 import "fmt"
 
 func Set() {
 	//fmt.Println("You have create a new Set!")
 	//fmt.Println("It just is a demo and updating...")
-	fmt.Println("I'm in set.Set()")
+	fmt.Println("I'm in set2.Set()")
 }
