e13cf21fc783df14d0e0a74d73907fc917d8bd4c
Jimmy <zhoumin@bupt.edu.cn>
2019-05-05 23:06:08 +0800

    fix bug in set.go

--- set.go
+++ set.go
@@ -14,6 +14,6 @@ func New() *Set {
 	//fmt.Println("You have create a new Set!")
 	//fmt.Println("It just is a demo and updating...")
 	fmt.Println("I'm in set.Set()")
-	return &Set{}
+	return &Set{make(map[string]int)}
 
 }
