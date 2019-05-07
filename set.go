6884697c284b7d82dca1e954c190a055ea6ac2f4
Jimmy <zhoumin@bupt.edu.cn>
2019-05-07 16:23:05 +0800

    add basic function for set.

--- set.go
+++ set.go
@@ -3,17 +3,24 @@ package set
 import "fmt"
 
 type Set struct {
-	values map[string]int
+	values map[interface{}]bool
 }
 
-func (s *Set) Add(val string) {
-	s.values[val] = 1
+func (s Set) Add(value interface{}) {
+	s.values[value] = true
 }
 
-func New() *Set {
+func (s Set) Length() int {
+	return len(s.values)
+}
+func (s Set) Show() {
+	for key, val := range s.values {
+		fmt.Printf("%v %t\n", key, val)
+	}
+}
+func New() Set {
 	//fmt.Println("You have create a new Set!")
 	//fmt.Println("It just is a demo and updating...")
-	fmt.Println("I'm in set.Set()")
-	return &Set{make(map[string]int)}
-
+	//fmt.Println("I'm in set.Set()")
+	return Set{make(map[interface{}]bool)}
 }
--- test/main.go
+++ test/main.go
@@ -3,7 +3,16 @@ package main
 import "fmt"
 import "../../set"
 
+func test_set_basic() {
+	s := set.New()
+	s.Add("hello")
+	s.Add(123)
+	s.Add('a')
+	s.Add(true)
+	fmt.Println(s.Length())
+	s.Show()
+}
+
 func main() {
-	fmt.Println("vim-go")
-	set.New()
+	test_set_basic()
 }
